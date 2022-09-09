0252a1974565b5e985f0773f88662acd4628dbdf
DavidSilverman <david.silverman@mymail.champlain.edu>
2022-09-09 13:40:55 -0400

    I am finally committing

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,9 +3,22 @@
 
 Vector2 CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
     Vector2 cohesionForce;
+    Vector2 centerOfMass;
 
     // todo: add your code here to make a force towards the center of mass
     // hint: iterate over the neighborhood
+
+    //centerOfMass = boid->transform.position;
+
+    for (int i = 0; i < neighborhood.size(); i++) 
+    {
+        centerOfMass += neighborhood[i]->getPosition();
+    }
+
+    centerOfMass.x /= (neighborhood.size());
+    centerOfMass.y /= (neighborhood.size());
+
+    cohesionForce = (centerOfMass - boid->transform.position);
 
     return cohesionForce;
 }
